962f18b1bb1947e035a1ca1c79a05db604180099
Safetyhat Lee <safetyhat621519@naver.com>
2024-05-16 11:35:35 +0000

    message

---
+++ micromod.java
@@ -0,0 +1,17 @@
+import java.time.*;
+
+class Micromod {
+    class User {
+        String username; int usercode;
+        boolean isMember;
+    }
+    class Vehicle {
+        int vhc; int[] loc;
+        double battery; int[][] pricetag; int hash;
+        
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
