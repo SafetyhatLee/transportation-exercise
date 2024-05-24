af3b7280bd623dd6670181c8ce4ef831bce705be
Safetyhat Lee <127012119+SafetyhatLee@users.noreply.github.com>
2024-05-24 23:37:29 +0900

    Update Users.java

--- micromod/Users.java
+++ micromod/Users.java
@@ -8,8 +8,28 @@ public abstract class Users {
 class Onedirect implements Users {
   public Onedirect() {
     userID = Random.randInt(0, 100000);
-    costParam = [1.4, ]
+    costParam = {800.0, 100.0, 2.5};
   }
   public void onProcess(String card) {cardNum = card;}
   
+}
+
+class Member implements Users {
+
+  static String membershipcard;
+  ArrayList<HashMap<String, Object>> badges;
+  String nickname; double mileage;
+  
+  public Onedirect(String card, boolean invited) {
+    userID = Random.randInt(0, 100000);
+    costParam = {800.0, 100.0, 2.5};
+    membershipcard = card;
+    badges = new ArrayList<>();
+    mileage = invited ? 1500.0 : 500.0;
+  }
+
+  public void setNickname(String nickname) {this.nickname = nickname;}
+  public void hangBadge(Badge bad) {
+    checkBadge(userID, bad) ? badges.add(bad) : System.err.println("This badge is not available for you");
+  }  
 }
